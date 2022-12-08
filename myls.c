419c9d9abb10df96f218b5b436ccf8504d2ac139
yousif anwar <yousifanwarrr@gmail.com>
2022-12-09 00:33:19 +0200

    adding new features like support options "-l -i -a -li -la -la1 ....."

--- myls.c
+++ myls.c
@@ -8,6 +8,23 @@
 #include <grp.h>
 #include <time.h>
 
+#define EXIST (1u)
+#define NOT_EXIST (0)
+
+typedef struct
+{
+	uint8_t l : 1;
+	uint8_t a : 1;
+	uint8_t R : 1;
+    uint8_t i : 1;
+    uint8_t _1 : 1;
+} optFlags_s;
+
+typedef union
+{
+	optFlags_s fields;
+	uint8_t whole_flags;
+} optFlags_u;
 
 /*struct of 16 bits of the permession of any file*/
 typedef struct
@@ -36,16 +53,110 @@ typedef union
 
 char file_type;
 char* month;
+
+char extract_options(int argc, char *argv[], optFlags_u *opt_flags, int *extra_args_index);
+void long_format_list_with_hidden_or_not(int argc, char **argv, char with_hidden, char with_inode);
+void normal_list(int argc, char **argv, char with_inode, char with_hidden, char separate);
+
+
 int main(int argc, char **argv)
 {
+    optFlags_u opt_flags = {.whole_flags = NOT_EXIST};
+	int extra_args_index = -1;
+char opt_state = extract_options(argc, argv, &opt_flags, &extra_args_index);
+
+	if (opt_state != 0)
+	{
+		printf("unknown option -%c\n", opt_state);
+		return -1;
+	}
+
+	if (opt_flags.whole_flags == 0)
+	{
+		normal_list(argc,argv,'0','0','0');
+	}
+    else if(opt_flags.whole_flags == 1){
+        long_format_list_with_hidden_or_not(argc, argv,'0','0');
+    }
+    else if(opt_flags.whole_flags == 3){
+        long_format_list_with_hidden_or_not(argc, argv,'1','0');
+    }
+    else if(opt_flags.whole_flags == 9){
+        long_format_list_with_hidden_or_not(argc, argv,'0','1');
+    }
+    else if(opt_flags.whole_flags == 11){
+        long_format_list_with_hidden_or_not(argc, argv,'1','1');
+    }
+    else if(opt_flags.whole_flags == 2){
+        normal_list(argc, argv,'0','1','0');
+    }
+    else if(opt_flags.whole_flags == 8){
+        normal_list(argc, argv,'1','0','0');
+    }
+    else if(opt_flags.whole_flags == 10){
+        normal_list(argc, argv,'1','1','0');
+    }
+    else if(opt_flags.whole_flags == 16){
+        normal_list(argc, argv,'0','0','1');
+    }
+    else if(opt_flags.whole_flags == 18){
+        normal_list(argc, argv,'0','1','1');
+    }
+    else if(opt_flags.whole_flags == 24){
+        normal_list(argc, argv,'1','0','1');
+    }
+    else if(opt_flags.whole_flags == 26){
+        normal_list(argc, argv,'1','1','1');
+    }
+
+    return 0;
+}
+
+
+char extract_options(int argc, char *argv[], optFlags_u *opt_flags, int *extra_args_index)
+{
+	opt_flags->whole_flags = NOT_EXIST;
+	char option;
+	char ret_val = 0;
+
+	while ((option = getopt(argc, argv, "lai1R")) != -1)
+	{
+		switch (option)
+		{
+		case 'l':
+			opt_flags->fields.l = EXIST;
+			break;
+		case 'a':
+			opt_flags->fields.a = EXIST;
+			break;
+		case 'R': //not used yet
+			opt_flags->fields.R = EXIST;
+			break;
+        case 'i':
+			opt_flags->fields.i = EXIST;
+			break;
+		case '1':
+			opt_flags->fields._1 = EXIST;
+			break;
+		case '?':
+			ret_val = optopt;
+			break;
+		}
+	}
+	*extra_args_index = optind;
+	return ret_val;
+}
+
+
+void long_format_list_with_hidden_or_not(int argc, char **argv, char with_hidden, char with_inode){
     char first = 0;
     char num_of_loops = 0;
     /*for loop for list the files in any dir of in the current path*/
-    for (int i = 1; i <= argc; i++)
+    for (int i = optind; i <= argc; i++)
     {
         DIR *dirptr;
         /*to list the files in the curr path*/
-        if(argc == 1){
+        if(optind == argc){
             char cwd_buf[100];
             char *cwd = getcwd(cwd_buf, 100);
             printf("CWD: %s\n", cwd);
@@ -66,6 +177,7 @@ int main(int argc, char **argv)
         struct stat mystat;
         while ((entry = readdir(dirptr)) != NULL)
         {
+            if((with_hidden == '0') && (entry->d_name[0] == '.')){continue;}
             if ((stat(entry->d_name, &mystat)) < 0)
             {
                 printf("stat failed to %s\n", entry->d_name);
@@ -145,6 +257,9 @@ int main(int argc, char **argv)
                 default:
                     break;
             }
+            if(with_inode == '1'){
+                printf("%7lu ", entry->d_ino);
+            }
             /*the output of the ls -la format*/
             printf("%c%c%c%c%c%c%c%c%c%c ",
                    file_type,
@@ -172,10 +287,63 @@ int main(int argc, char **argv)
         }
         puts(""); // separation between the content of each dir
         closedir(dirptr);
-        if(num_of_loops == (argc - 1)){
+        if(i == (argc - 1)){
             break;
         }
     }
+}
 
-    return 0;
+void normal_list(int argc, char **argv, char with_inode, char with_hidden, char separate){
+    char first = 0;
+    char num_of_loops = 0;
+    /*for loop for list the files in any dir of in the current path*/
+    for (int i = optind; i <= argc; i++)
+    {
+        DIR *dirptr;
+        /*to list the files in the curr path*/
+        if(optind == argc){
+            char cwd_buf[100];
+            char *cwd = getcwd(cwd_buf, 100);
+            printf("CWD: %s\n", cwd);
+            dirptr = opendir(cwd);
+        }
+        /*to list all files in the dir which passed to the myls command*/
+        else{
+            num_of_loops++;
+            if(first != 0){
+                chdir("../");
+            }
+            first = 1;
+            printf("%s:\n", argv[i]);
+            dirptr = opendir(argv[i]);
+            chdir(argv[i]);
+            }
+        struct dirent *entry;
+        struct stat mystat;
+        while ((entry = readdir(dirptr)) != NULL)
+        {
+            if((with_hidden == '0') && (entry->d_name[0] == '.')){continue;}
+            if(with_inode == '0' && separate == '0'){
+                printf("%s  ", entry->d_name);
+            }
+            if(with_inode == '1' && separate == '0'){
+                printf("%lu %s  ", entry->d_ino, entry->d_name);
+            }
+            if(with_inode == '1' && separate == '1'){
+                printf("%lu %s\n", entry->d_ino, entry->d_name);
+            }
+            if(separate == '1' && with_inode == '0'){
+                printf("%s  \n", entry->d_name);
+            }
+
+
+        
+        }
+        puts(""); // separation between the content of each dir
+        puts(""); // separation between the content of each dir
+        closedir(dirptr);
+        if(i == (argc - 1)){
+            break;
+        }
+}
 }
